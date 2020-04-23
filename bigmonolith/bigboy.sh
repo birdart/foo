b7c5bc9f77dc24bd59136a4586ee86ecfbb8c513
Alex Bird <bird@qualia.com>
2020-04-22 17:11:50 -0700

    add generator script

--- bigmonolith/bigboy.sh
+++ bigmonolith/bigboy.sh
@@ -3,8 +3,8 @@
 rm -rf docs
 mkdir -p docs
 cd docs
-for i in $(seq 100) ; do
-  dd if=/usr/share/dict/words of=doc${i} bs=1MB count=16
+for i in $(seq 10) ; do
+  dd if=/dev/random of=doc${i} bs=1MB count=16
 done
 
 cd ..
